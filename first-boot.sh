1f27dcac4cb469b3795b3388137d4ec354df25a4
Jesus Elias <jelias@navi.local>
2019-08-03 20:50:31 -0700

    updated script
    now configures apple remote desktop access through group.
    optional:
    set computer name dialog
    set hostname dialog
    install munki tools dialog

--- first-boot.sh
+++ first-boot.sh
@@ -1,44 +1,90 @@
-#!/bin/sh
+#!/bin/bash
 
-#set computer name
-sudo systemsetup -setcomputername NAME
+company_name="Company Name"
+admin_user="administrator"
+net_time="TIME_SERVER"
+ard_kickstart="/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart"
+dns_servers="0.0.0.0 1.1.1.1"
+dns_search="sub1.domain.net sub2.domain.net"
+ard_group_name="com.apple.local.ard_admin"
+OLDIFS=$IFS; IFS=$'\n'
 
-#set hostname
-sudo scutil --set HostName NAME.local
+# get computer name
+#computer_name=$(/usr/bin/osascript -e 'display dialog "Computer Name" default answer "John.Smith.001-100-00000001" buttons {"Cancel", "Continue"} default button 2 with title "Client Setup"' -e 'text returned of the result')
+#if [[ $computer_name != "" ]]; then
+#	/usr/sbin/scutil --set ComputerName $computer_name
+#fi
 
-#set sleep schedule to 30 minutes to all power sources
-sudo pmset -a displaysleep 30 disksleep 30 sleep 30 womp 1 autorestart 1
+# get host name
+#host_name=$(/usr/bin/osascript -e 'display dialog "Host Name" default answer "smi100" buttons {"Cancel", "Continue"} default button 2 with title "Client Setup"' -e 'text returned of the result')
+#if [[ $host_name != "" ]]; then
+#	/usr/sbin/scutil --set HostName $host_name
+#	/usr/sbin/scutil --set LocalHostName $host_name
+#fi
 
-#disable user list at login window
-sudo defaults write /Library/Preferences/com.apple.loginwindow.plist SHOWFULLNAME -bool true
-
-#add branding at login window
-sudo defaults write /Library/Preferences/com.apple.loginwindow LoginwindowText "Property of COMPANY NAME"
-
-#enable fast user switching
-sudo defaults write /Library/Preferences/.GlobalPreferences MultipleSessionEnabled -bool true
-
-#hide the local admin
-sudo dscl . -create /Users/ADMIN_USER IsHidden 1
-if [ -d /Users/ADMIN_USER ]; then
-    sudo mv /Users/ADMIN_USER /var/ADMIN_USER
+# set ard
+ard_check=$(/usr/bin/dscl /Local/Default -list /Groups | /usr/bin/grep "$ard_group_name")
+if [[ $ard_check == "" ]]; then
+	/usr/sbin/dseditgroup -o create -n /Local/Default -r "Apple Remote Desktop Administrators" "$ard_group_name"
+	/usr/sbin/dseditgroup -o edit -n /Local/Default -a animation -t user "$ard_group_name"
+	if [[ $computer_name != "" ]] && [[ $host_name != "" ]]; then
+		ard_text1=$(/bin/echo $computer_name | /usr/bin/grep -o '.\{8\}$')
+		ard_text2=$(/bin/echo $computer_name | /usr/bin/cut -d'.' -f'3' | /usr/bin/cut -d'-' -f'1,2')
+		ard_text3=$host_name
+		$ard_kickstart -quiet -configure -computerinfo -set1 -1 "${ard_text1}" -set2 -2 "${ard_text2}" -set3 -3 "${ard_text3}"
+	fi
+	$ard_kickstart -quiet -configure -allowAccessFor -specifiedUsers
+	$ard_kickstart -activate -configure -setmenuextra -menuextra  no -clientopts -setdirlogins -dirlogins yes
 fi
-sudo dscl . -create /Users/ADMIN_USER NFSHomeDirectory /var/ADMIN_USER
 
-#disable mac app store auto update
-sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate AutomaticCheckEnabled -bool false
+# set dns
+for i in $(/usr/sbin/networksetup -listallnetworkservices | /usr/bin/grep 'Ethernet'); do 
+	IFS=$OLDIFS
+	/usr/sbin/networksetup -setdnsservers "$i" $dns_servers
+	/usr/sbin/networksetup -setsearchdomains "$i" $dns_search
+done
 
-#disable java auto update
-sudo defaults write /Library/Preferences/com.oracle.java.Java-Updater JavaAutoUpdateEnabled -bool false
+# set network time
+/usr/sbin/systemsetup -setnetworktimeserver $net_time
+/usr/sbin/systemsetup -setusingnetworktime on
 
-#disable flash auto update
-if [ ! -f /Library/Application\ Support/Macromedia/mms.cfg ]; then
-mkdir /Library/Application\ Support/Macromedia
-touch /Library/Application\ Support/Macromedia/mms.cfg
-chmod 644 /Library/Application\ Support/Macromedia/mms.cfg
-CONF="/Library/Application Support/Macromedia/mms.cfg"
-cat > "${CONF}" << EOF
-AutoUpdateDisable=1
-SilentAutoUpdateEnable=0
-EOF
+# set energy saver
+/usr/bin/pmset -a displaysleep 15 disksleep 0 sleep 0 powernap 0 autorestart 1 womp 1
+
+# set login options
+/usr/bin/defaults write /Library/Preferences/.GlobalPreferences MultipleSessionEnabled -bool true
+/usr/bin/defaults write /Library/Preferences/com.apple.loginwindow SHOWFULLNAME -bool false
+/usr/bin/defaults write /Library/Preferences/com.apple.loginwindow LoginwindowText "Property of $company_name"
+
+# add all users to _lpoperator
+/usr/sbin/dseditgroup -o edit -t group -a everyone _lpoperator
+
+# disable connect to server confirmation
+/usr/bin/defaults write /Library/Preferences/com.apple.NetworkAuthorization AllowUnknownServers -bool true
+
+# hide local admin
+/usr/bin/dscl /Local/Default -create /Users/$admin_user IsHidden 1
+if [[ -d /Users/$admin_user ]]; then
+	/bin/mv -f /Users/$admin_user /var/$admin_user
 fi
+/usr/bin/dscl /Local/Default -create /Users/$admin_user NFSHomeDirectory /var/$admin_user
+
+# disable java and flash auto updates
+#/usr/bin/defaults write /Library/Preferences/com.oracle.java.Java-Updater JavaAutoUpdateEnabled -bool false
+#if [[ ! -f /Library/Application\ Support/Macromedia/mms.cfg ]]; then
+#	/bin/mkdir /Library/Application\ Support/Macromedia
+#	/usr/bin/touch /Library/Application\ Support/Macromedia/mms.cfg
+#	/bin/echo "AutoUpdateDisable=1" >> /Library/Application\ Support/Macromedia/mms.cfg
+#	/bin/echo "SilentAutoUpdateEnable=0" >> /Library/Application\ Support/Macromedia/mms.cfg
+#	/bin/chmod 644 /Library/Application\ Support/Macromedia/mms.cfg
+#fi
+
+# install munkitools
+#install_munki=$(/usr/bin/osascript -e 'display dialog "Would you like to install the Munki client?" buttons {"No", "Yes"} default button 2 with title "Client Setup"' -e 'button returned of the result')
+#if [[ $install_munki == "Yes" ]]; then
+#	/usr/sbin/installer -pkg /private/tmp/munki_tools.pkg -target /
+#	/usr/bin/touch /Users/Shared/.com.googlecode.munki.checkandinstallatstartup
+#fi
+#/bin/rm -f /private/tmp/munki_tools.pkg
+
+exit 0
